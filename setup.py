67bbdcc99ef406d4d870460547afb51814eefdc8
Gabriel Caccaos <gabriel.caccaos@gmail.com>
2020-03-13 18:17:09 -0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='dialogflow-fulfillment-python',
+    version='0.0.1.dev1',
+    author='Gabriel F. Caccaos',
+    author_email='gabriel.caccaos@gmail.com',
+    packages=['dialogflow_fulfillment'],
+    # url='',
+    # license='',
+    description='Dialogflow Fulfillment Library',
+    long_description=open('README.md').read(),
+    python_requires='>=3',
+)
