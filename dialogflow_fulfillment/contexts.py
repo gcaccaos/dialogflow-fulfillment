8cc0a40f9f734a8a18756c698df3a92cea97202c
Gabriel Caccaos <gabriel.caccaos@gmail.com>
2020-03-17 19:21:09 -0300

    Add contexts.py

---
+++ dialogflow_fulfillment/contexts.py
@@ -0,0 +1,63 @@
+class Context:
+    def __init__(self, input_contexts, session):
+        self.session = session
+        self.input_contexts = self._process_input_contexts(input_contexts)
+        self.contexts = self._process_input_contexts(input_contexts)
+
+    @staticmethod
+    def _process_input_contexts(input_contexts):
+        """Processes a list of Dialogflow input contexts"""
+        contexts = {}
+
+        for context in input_contexts:
+            name = context['name'].rsplit('/', 1)[-1]
+            contexts[name] = context
+
+        return contexts
+
+    def set_(self, name, lifespan_count=None, parameters=None):
+        """Set a new Dialogflow outgoing context"""
+        if not isinstance(name, str):
+            raise TypeError('name argument must be a string')
+
+        if name not in self.contexts:
+            self.contexts[name] = {'name': name}
+
+        if lifespan_count is not None:
+            self.contexts[name]['lifespanCount'] = lifespan_count
+
+        if parameters is not None:
+            self.contexts[name]['parameters'] = parameters
+
+    def get(self, name):
+        """Get an context from the Dialogflow webhook request"""
+        return self.contexts[name]
+
+    def delete(self, name):
+        """Delete an context a Dialogflow session (set the lifespan to 0)"""
+        self.set_(name, lifespan_count=0)
+
+    def get_output_contexts_array(self):
+        """Get array of context objects"""
+        output_contexts = [*self]
+
+        for context in output_contexts:
+            context['name'] = f"{self.session}/contexts/{context['name']}"
+        
+        return output_contexts
+    
+    def __iter__(self):
+        self._index = 0
+        self._context_array = list(self.contexts.values())
+
+        return self
+    
+    def __next__(self):
+        if self._index >= len(self._context_array):
+            raise StopIteration
+
+        context = self._context_array[self._index]
+
+        self._index += 1
+
+        return context
