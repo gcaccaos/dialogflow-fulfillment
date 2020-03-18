cb30173a380f7bb65c911fddc82ea03d86508915
Gabriel Caccaos <gabriel.caccaos@gmail.com>
2020-03-17 22:07:57 -0300

    Add module docstring to __init__.py

--- dialogflow_fulfillment/__init__.py
+++ dialogflow_fulfillment/__init__.py
@@ -1,3 +1,4 @@
+"""A Dialogflow's webhook fulfillment API v2 library for Python"""
 from .contexts import Context
 from .rich_responses import QuickReplies, RichResponse, Text
 from .webhook_client import WebhookClient
