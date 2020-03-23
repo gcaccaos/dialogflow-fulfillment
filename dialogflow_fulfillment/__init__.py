82926c878b216cc38f53edd36226f60c879d217f
Gabriel Caccaos <gabriel.caccaos@gmail.com>
2020-03-23 11:50:58 -0300

    Add RichResponse classes imports to __init__.py

--- dialogflow_fulfillment/__init__.py
+++ dialogflow_fulfillment/__init__.py
@@ -1,4 +1,4 @@
 """A Dialogflow's webhook fulfillment API v2 library for Python"""
 from .contexts import Context
-from .rich_responses import QuickReplies, RichResponse, Text
+from .rich_responses import Image, Payload, QuickReplies, RichResponse, Text
 from .webhook_client import WebhookClient
