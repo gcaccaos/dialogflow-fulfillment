50446127dcda54fb2201525ca02b7cc51af9de45
Gabriel Caccaos <gabriel.caccaos@gmail.com>
2020-03-26 21:33:39 -0300

    Update RichResponse's docstring

--- dialogflow_fulfillment/rich_responses/rich_response.py
+++ dialogflow_fulfillment/rich_responses/rich_response.py
@@ -1,7 +1,7 @@
 """Module for base rich response"""
 
 class RichResponse:  # pylint: disable=too-few-public-methods
-    """Dialogflow's Rich Response class"""
+    """Dialogflow's rich response"""
 
     def _get_response_object(self):
         """Gets the v2 response object"""
