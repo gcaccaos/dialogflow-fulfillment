ee8c821f8360674a5205effb1093f190118330d8
Gabriel Caccaos <gabriel.caccaos@gmail.com>
2020-03-26 23:21:27 -0300

    Update version in setup()

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dialogflow-fulfillment',
-    version='0.0.1',
+    version='0.1.0',
     author='Gabriel F. Caccaos',
     author_email='gabriel.caccaos@gmail.com',
     packages=['dialogflow_fulfillment'],
