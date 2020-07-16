30f1094261db2e21f7e5f0f86e5073cb7f9a8116
Gabriel Caccaos <gabriel.caccaos@gmail.com>
2020-07-16 00:02:15 -0300

    Add simple example file

---
+++ samples/simple_example.py
@@ -0,0 +1,19 @@
+from dialogflow_fulfillment import QuickReplies, WebhookClient
+
+
+# Define a custom handler function
+def handler(agent: WebhookClient) -> None:
+    """
+    This handler sends a text message along with a quick replies message
+    back to Dialogflow, which uses the messages to build the final response
+    to the user.
+    """
+    agent.add('How are you feeling today?')
+    agent.add(QuickReplies(quick_replies=['Happy :)', 'Sad :(']))
+
+
+# Create an instance of the WebhookClient
+agent = WebhookClient(request)
+
+# Handle the request using the handler function
+agent.handle_request(handler)
