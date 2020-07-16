03eff4f3f33fee929d4493cf9835996e7f6abff1
Gabriel Caccaos <gabriel.caccaos@gmail.com>
2020-07-16 19:07:38 -0300

    Wrap docstring in simple_example.py

--- samples/simple_example.py
+++ samples/simple_example.py
@@ -4,9 +4,9 @@ from dialogflow_fulfillment import QuickReplies, WebhookClient
 # Define a custom handler function
 def handler(agent: WebhookClient) -> None:
     """
-    This handler sends a text message along with a quick replies message
-    back to Dialogflow, which uses the messages to build the final response
-    to the user.
+    This handler sends a text message along with a quick replies 
+    message back to Dialogflow, which uses the messages to build
+    the final response to the user.
     """
     agent.add('How are you feeling today?')
     agent.add(QuickReplies(quick_replies=['Happy :)', 'Sad :(']))
