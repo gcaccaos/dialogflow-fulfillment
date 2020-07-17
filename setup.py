2bb3e4be856299887c58e74e5f923eba704d5b2a
Gabriel Caccaos <gabriel.caccaos@gmail.com>
2020-07-17 14:42:04 -0300

    Bump version to 0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dialogflow-fulfillment',
-    version='0.1.3',
+    version='0.1.4',
     author='Gabriel Farias Caccáos',
     author_email='gabriel.caccaos@gmail.com',
     packages=['dialogflow_fulfillment'],
