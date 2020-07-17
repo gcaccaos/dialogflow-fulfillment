61da2d0d43f8bc4e8d31766cf6935fd9e6c40fcf
Gabriel Caccaos <gabriel.caccaos@gmail.com>
2020-07-17 19:11:09 -0300

    Rewrite tests with pytest

--- tests/test_rich_responses.py
+++ tests/test_rich_responses.py
@@ -1,101 +1,118 @@
-"""Unit tests module for RichResponse classes"""
-import unittest
+import pytest
 
 from dialogflow_fulfillment import Card, Image, Payload, QuickReplies, Text
 
 
-class TextTests(unittest.TestCase):
-    def setUp(self):
-        self.text = Text('text')
-
-    def test_has_text(self):
-        self.assertTrue(hasattr(self.text, 'text'))
-
-    def test_no_arg(self):
-        self.assertRaises(TypeError, Text)
-
-    def test_non_string_arg(self):
-        self.assertRaises(TypeError, Text, ['list'])
-
-    def test_get_response_object(self):
-        self.assertEqual({'text': {'text': [self.text.text]}},
-                         self.text._get_response_object())
+# Tests for Text response
+@pytest.fixture
+def text():
+    return 'this is a text'
 
 
-class QuickRepliesTests(unittest.TestCase):
-    def setUp(self):
-        self.quick_replies = QuickReplies(['first reply', 'second reply'])
+def test_text_attr(text):
+    text_obj = Text(text)
 
-    def test_has_quick_replies(self):
-        self.assertTrue(hasattr(self.quick_replies, 'quick_replies'))
-
-    def test_no_arg(self):
-        self.assertRaises(TypeError, QuickReplies)
-
-    def test_non_sequence(self):
-        self.assertRaises(TypeError, QuickReplies, 'text')
-
-    def test_get_response_object(self):
-        self.assertEqual({'quickReplies': {'quickReplies': self.quick_replies.quick_replies}},
-                         self.quick_replies._get_response_object())
+    assert text_obj.text == text
 
 
-class PayloadTests(unittest.TestCase):
-    def setUp(self):
-        self.payload = Payload({'pay': 'load'})
-
-    def test_has_payload(self):
-        self.assertTrue(hasattr(self.payload, 'payload'))
-
-    def test_has_set_payload(self):
-        self.assertTrue(hasattr(self.payload, 'set_payload'))
-
-    def test_no_arg(self):
-        self.assertRaises(TypeError, Payload)
-
-    def test_non_dictionary(self):
-        self.assertRaises(TypeError, Payload, 'text')
-
-    def test_get_response_object(self):
-        self.assertEqual({'payload': self.payload.payload},
-                         self.payload._get_response_object())
+def test_text_non_string(text):
+    with pytest.raises(TypeError):
+        Text({'this': ['is not a text']})
 
 
-class ImageTests(unittest.TestCase):
-    def setUp(self):
-        self.image = Image('https://picsum.photos/128')
+def test_text_as_dict(text):
+    text_obj = Text(text)
 
-    def test_has_image_url(self):
-        self.assertTrue(hasattr(self.image, 'image_url'))
-
-    def test_has_set_image(self):
-        self.assertTrue(hasattr(self.image, 'set_image'))
-
-    def test_no_arg(self):
-        self.assertRaises(TypeError, Image)
-
-    def test_get_response_object(self):
-        self.assertEqual({'image': {'imageUri': self.image.image_url}},
-                         self.image._get_response_object())
+    assert text_obj._get_response_object() == {'text': {'text': [text]}}
 
 
-class CardTests(unittest.TestCase):
-    def setUp(self):
-        self.card = Card('card title')
-
-    def test_has_title(self):
-        self.assertTrue(hasattr(self.card, 'title'))
-
-    def test_has_set_title(self):
-        self.assertTrue(hasattr(self.card, 'set_title'))
-
-    def test_no_arg(self):
-        self.assertRaises(TypeError, Card)
-
-    def test_get_response_object(self):
-        self.assertEqual({'card': {'title': self.card.title}},
-                         self.card._get_response_object())
+# Tests for QuickReplies response
+@pytest.fixture
+def quick_replies():
+    return ['reply 1', 'reply 2', 'reply 3']
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_quick_replies_attr(quick_replies):
+    quick_replies_obj = QuickReplies(quick_replies)
+
+    assert quick_replies_obj.quick_replies == quick_replies
+
+
+def test_quick_replies_non_sequence():
+    with pytest.raises(TypeError):
+        QuickReplies('this is not a sequence')
+
+
+def test_quick_replies_as_dict(quick_replies):
+    quick_replies_obj = QuickReplies(quick_replies)
+
+    assert quick_replies_obj._get_response_object() == {'quickReplies': {'quickReplies': quick_replies}}
+
+
+# Tests for Payload response
+@pytest.fixture
+def payload():
+    return {'test key 1': 'test value 1', 'test key 2': 'test value 2'}
+
+
+def test_payload_attr(payload):
+    payload_obj = Payload(payload)
+
+    assert payload_obj.payload == payload
+
+
+def test_payload_non_dict():
+    with pytest.raises(TypeError):
+        Payload('this is not a dict')
+
+
+def test_payload_as_dict(payload):
+    payload_obj = Payload(payload)
+
+    assert payload_obj._get_response_object() == {'payload': payload}
+
+
+# Tests for Image response
+@pytest.fixture
+def image_url():
+    return 'https://test.url/image.jpg'
+
+
+def test_image_url_attr(image_url):
+    image_obj = Image(image_url)
+
+    assert image_obj.image_url == image_url
+
+
+def test_image_url_non_string():
+    with pytest.raises(TypeError):
+        Image({'this': ['is not a string']})
+
+
+def test_image_as_dict(image_url):
+    image_obj = Image(image_url)
+
+    assert image_obj._get_response_object() == {'image': {'imageUri': image_url}}
+
+
+# Tests for Card response
+@pytest.fixture
+def title():
+    return 'this is a title'
+
+
+def test_title_attr(title):
+    card_obj = Card(title)
+
+    assert card_obj.title == title
+
+
+def test_title_non_string():
+    with pytest.raises(TypeError):
+        Card({'this': ['is not a string']})
+
+
+def test_card_as_dict(title):
+    card_obj = Card(title)
+
+    assert card_obj._get_response_object() == {'card': {'title': title}}
