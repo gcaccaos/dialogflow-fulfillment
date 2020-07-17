3a7e0ecf6035db942ddb7d4b2b163bdd03bf8b6c
Gabriel Caccaos <gabriel.caccaos@gmail.com>
2020-07-17 00:38:31 -0300

    Define package's public API

--- dialogflow_fulfillment/__init__.py
+++ dialogflow_fulfillment/__init__.py
@@ -2,3 +2,14 @@ from .contexts import Context
 from .rich_responses import (Card, Image, Payload, QuickReplies, RichResponse,
                              Text)
 from .webhook_client import WebhookClient
+
+__all__ = [
+    'Context',
+    'Card',
+    'Image',
+    'Payload',
+    'QuickReplies',
+    'RichResponse',
+    'Text',
+    'WebhookClient',
+]
