06dfea752a143e6ef3639a6d8c9e5f02dc57e9d2
Gabriel Caccaos <gabriel.caccaos@gmail.com>
2020-07-17 21:26:14 -0300

    Add tests for Context and WebhookClient

---
+++ tests/test_contexts.py
@@ -0,0 +1,85 @@
+from uuid import uuid4
+
+import pytest
+
+from dialogflow_fulfillment import Context
+
+
+@pytest.fixture
+def session():
+    return str(uuid4())
+
+
+def test_context_get(session):
+    contexts = [
+        {
+            'name': f'projects/PROJECT_ID/agent/sessions/{session}/contexts/__system_counters__',
+            'parameters': {
+                'no-input': 0,
+                'no-match': 0
+            }
+        }
+    ]
+
+    context_api = Context(contexts, session)
+
+    assert context_api.get('a undefined context') is None
+
+
+def test_context_delete(session):
+    contexts = [
+        {
+            'name': f'projects/PROJECT_ID/agent/sessions/{session}/contexts/__system_counters__',
+            'parameters': {
+                'no-input': 0,
+                'no-match': 0
+            }
+        },
+        {
+            'name': f'projects/PROJECT_ID/agent/sessions/{session}/contexts/another_context',
+            'lifespanCount': 1,
+            'parameters': {
+                'no-input': 0,
+                'no-match': 0
+            }
+        }
+    ]
+
+    context_api = Context(contexts, session)
+
+    context_api.delete('another_context')
+
+    assert context_api.get('another_context')['lifespanCount'] == 0
+
+
+def test_context_set_non_string(session):
+    context_api = Context([], session)
+
+    with pytest.raises(TypeError):
+        context_api.set({'this': 'is not a string'})
+
+
+def test_context_set_new_context(session):
+    context_api = Context([], session)
+
+    context_api.set('new_context')
+
+    assert 'new_context' in context_api.contexts
+
+
+def test_context_set_parameters(session):
+    contexts = [
+        {
+            'name': f'projects/PROJECT_ID/agent/sessions/{session}/contexts/__system_counters__',
+            'parameters': {
+                'no-input': 0,
+                'no-match': 0
+            }
+        },
+    ]
+
+    context_api = Context(contexts, session)
+
+    context_api.set('__system_counters__', parameters={})
+
+    assert context_api.get('__system_counters__')['parameters'] == {}
---
+++ tests/test_webhook_client.py
@@ -0,0 +1,214 @@
+from uuid import uuid4
+
+import pytest
+
+from dialogflow_fulfillment import WebhookClient
+
+
+@pytest.fixture
+def webhook_request():
+    response_id = str(uuid4())
+    session = str(uuid4())
+    project_id = 'PROJECT_ID'
+    intent_id = str(uuid4())
+
+    return {
+        "responseId": response_id,
+        "queryResult": {
+            "queryText": "Hi",
+            "parameters": {},
+            "allRequiredParamsPresent": True,
+            "fulfillmentText": "Hello! How can I help you?",
+            "fulfillmentMessages": [
+                {
+                    "text": {
+                        "text": [
+                            "Hello! How can I help you?"
+                        ]
+                    }
+                },
+                {
+                    'payload': {
+                        'test key 1': 'test value 1',
+                        'test key 2': 'test value 2'
+                    }
+                },
+                {
+                    'quickReplies': {
+                        'quickReplies': [
+                            'quick reply 1',
+                            'quick reply 2',
+                            'quick reply 3'
+                        ]
+                    }
+                }
+            ],
+            "outputContexts": [
+                {
+                    "name": f"projects/{project_id}/agent/sessions/{session}/contexts/__system_counters__",
+                    "parameters": {
+                        "no-input": 0,
+                        "no-match": 0
+                    }
+                }
+            ],
+            "intent": {
+                "name": f"projects/{project_id}/agent/intents/{intent_id}",
+                "displayName": "Default Welcome Intent"
+            },
+            "intentDetectionConfidence": 1,
+            "languageCode": "en"
+        },
+        "originalDetectIntentRequest": {
+            "payload": {},
+        },
+        "session": f"projects/{project_id}/agent/sessions/{session}"
+    }
+
+
+def test_webhook_client_non_dict():
+    with pytest.raises(TypeError):
+        WebhookClient('this is not a dict')
+
+
+def test_webhook_client_add_text(webhook_request):
+    agent = WebhookClient(webhook_request)
+
+    def handler(agent):
+        agent.add('this is a text')
+
+    agent.handle_request(handler)
+
+    assert agent.response == {
+        'fulfillmentMessages': [{'text': {'text': ['this is a text']}}],
+        'outputContexts': [*webhook_request['queryResult']['outputContexts']]
+    }
+
+
+def test_webhook_client_add_list_of_texts(webhook_request):
+    agent = WebhookClient(webhook_request)
+
+    def handler(agent):
+        agent.add(['this', 'is', 'a', 'list', 'of', 'texts'])
+
+    agent.handle_request(handler)
+
+    assert agent.response == {
+        'fulfillmentMessages': [
+            {'text': {'text': ['this']}},
+            {'text': {'text': ['is']}},
+            {'text': {'text': ['a']}},
+            {'text': {'text': ['list']}},
+            {'text': {'text': ['of']}},
+            {'text': {'text': ['texts']}}
+        ],
+        'outputContexts': [*webhook_request['queryResult']['outputContexts']]
+    }
+
+
+def test_webhook_client_add_non_richresponse(webhook_request):
+    agent = WebhookClient(webhook_request)
+
+    def handler(agent):
+        agent.add({'this': 'is not a RichResponse'})
+
+    with pytest.raises(TypeError):
+        agent.handle_request(handler)
+
+
+def test_webhook_client_set_followup_event(webhook_request):
+    agent = WebhookClient(webhook_request)
+
+    def handler(agent):
+        agent.set_followup_event('test_event')
+
+    agent.handle_request(handler)
+
+    assert agent.response == {
+        'outputContexts': [*webhook_request['queryResult']['outputContexts']],
+        'followupEventInput': {
+            'name': 'test_event',
+            'languageCode': webhook_request['queryResult']['languageCode']
+        }
+    }
+
+
+def test_webhook_client_set_followup_event_by_dict(webhook_request):
+    agent = WebhookClient(webhook_request)
+
+    def handler(agent):
+        agent.set_followup_event({'name': 'test_event'})
+
+    agent.handle_request(handler)
+
+    assert agent.response['followupEventInput'] == {
+        'name': 'test_event',
+        'languageCode': webhook_request['queryResult']['languageCode']
+    }
+
+
+def test_webhook_client_handler_intent_map(webhook_request):
+    agent = WebhookClient(webhook_request)
+
+    handler = {
+        'Default Welcome Intent': lambda agent: agent.add('Hello!'),
+        'Default Fallback Intent': lambda agent: agent.add('Sorry, I\'m having trouble to understand you...'),
+    }
+
+    agent.handle_request(handler)
+
+    assert agent.response == {
+        'fulfillmentMessages': [{'text': {'text': ['Hello!']}}],
+        'outputContexts': [*webhook_request['queryResult']['outputContexts']],
+    }
+
+
+def test_webhook_client_non_callable_handler(webhook_request):
+    agent = WebhookClient(webhook_request)
+
+    handler = 'this is not a callable'
+
+    with pytest.raises(TypeError):
+        agent.handle_request(handler)
+
+
+def test_webhook_client_no_contexts(webhook_request):
+    modified_webhook_request = webhook_request
+    modified_webhook_request['queryResult']['outputContexts'] = []
+
+    agent = WebhookClient(modified_webhook_request)
+
+    def handler(agent):
+        pass
+
+    agent.handle_request(handler)
+
+    assert 'outputContexts' not in agent.response
+
+
+def test_webhook_client_with_request_source(webhook_request):
+    modified_webhook_request = webhook_request
+    modified_webhook_request['originalDetectIntentRequest']['source'] = 'PLATFORM_UNSPECIFIED'
+
+    agent = WebhookClient(modified_webhook_request)
+
+    def handler(agent):
+        pass
+
+    agent.handle_request(handler)
+
+    assert agent.response['source'] == 'PLATFORM_UNSPECIFIED'
+
+
+def test_webhook_client_with_unknown_message(webhook_request):
+    modified_webhook_request = webhook_request
+    modified_webhook_request['queryResult']['fulfillmentMessages'] = [
+        {
+            'foo': {
+                'foo': ['bar']
+            }
+        }
+    ]
+
+    with pytest.raises(TypeError):
+        WebhookClient(modified_webhook_request)
