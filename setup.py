ddf8783b9365471d11f9227f30845aee971fe82b
Gabriel Caccaos <gabriel.caccaos@gmail.com>
2020-09-24 15:30:09 -0300

    Configure setup to include package data

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     description=description,
     long_description_content_type='text/markdown',
     long_description=open('README.md').read(),
+    include_package_data=True,
     python_requires='>=3',
     extras_require={
         'dev': [
