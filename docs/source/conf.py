51c668859243c6c688220e053a12e51f8719fc6b
Gabriel Farias Caccaos <8305083+gcaccaos@users.noreply.github.com>
2022-04-17 00:42:04 -0300

    Change sphinx's theme to furo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,7 +39,6 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
-    'sphinx_rtd_theme',
     'sphinxcontrib.mermaid',
 ]
 
@@ -57,19 +56,13 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
-html_theme_options = {
-    'sticky_navigation': True,
-}
+html_theme = 'furo'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 
-html_css_files = [
-    'custom.css',
-]
 
 # -- Other settings ----------------------------------------------------------
 
